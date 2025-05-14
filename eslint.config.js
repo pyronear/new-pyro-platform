55845467f9cad47ba87cb7739af3aff155e5c8db
alexis <alexis.reymann@theodo.com>
2025-05-14 14:40:40 +0200

    add format script + eslint vite plugin

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ import reactX from 'eslint-plugin-react-x';
 import reactDom from 'eslint-plugin-react-dom';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'node_modules/', '*.test.js'] },
   {
     extends: [
       js.configs.recommended,
