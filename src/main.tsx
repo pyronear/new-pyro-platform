602ef9bfe5f6ba0214d3946a44d24dedd02aeb0e
Yoctoboy <alexis.reymann@hotmail.fr>
2025-06-02 19:58:44 +0200

    improve prettier/eslint configs
    they now work together, i.e. prettier runs like an eslint rule that's auto-fixable

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App.tsx';
-import { CssBaseline, ThemeProvider } from '@mui/material';
 import { theme } from './theme.ts';
 
 const rootElement = document.getElementById('root');
