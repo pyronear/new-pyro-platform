1acf464cebd732a4a81514fb08fcacb0982715db
Yoctoboy <alexis.reymann@hotmail.fr>
2025-06-22 20:09:05 +0200

    add Authprovider in root app

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,22 @@ import { Box } from '@mui/material';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import { Topbar } from './components/Topbar/Topbar';
+import { AuthProvider } from './context/AuthProvider';
 import { LoginPage } from './pages/LoginPage';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Topbar />
-      <Box height="calc(100vh - 64px)">
-        <Routes>
-          <Route path="/about" element={<>This is the about page</>}></Route>
-          <Route path="/" element={<LoginPage />}></Route>
-        </Routes>
-      </Box>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Topbar />
+        <Box height="calc(100vh - 64px)">
+          <Routes>
+            <Route path="/dashboard" element={<>You are logged in</>}></Route>
+            <Route path="/" element={<LoginPage />}></Route>
+          </Routes>
+        </Box>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
