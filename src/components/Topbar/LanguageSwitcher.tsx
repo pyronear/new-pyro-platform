71ef20b7147cbffc3ceae981c19f565d7d036306
fe51 <55736935+fe51@users.noreply.github.com>
2025-07-07 10:49:19 +0200

    Add Spanish and flags

--- src/components/Topbar/LanguageSwitcher.tsx
+++ src/components/Topbar/LanguageSwitcher.tsx
@@ -3,9 +3,14 @@ import {
   MenuItem,
   Select,
   type SelectChangeEvent,
+  Stack,
 } from '@mui/material';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+
+import es from '../../assets/languages/es.svg';
+import fr from '../../assets/languages/fr.svg';
+import gb from '../../assets/languages/gb.svg';
 
 const LanguageSwitcher: React.FC = () => {
   const { i18n } = useTranslation();
@@ -26,10 +31,26 @@ const LanguageSwitcher: React.FC = () => {
           height: '30px',
         })}
       >
-        <MenuItem value="en">ENGLISH</MenuItem>
-        <MenuItem value="fr">FRANÇAIS</MenuItem>
+        <MenuItem value="en">
+          <StackLanguage label="ENGLISH" icon={gb} />
+        </MenuItem>
+        <MenuItem value="fr">
+          <StackLanguage label="FRANÇAIS" icon={fr} />
+        </MenuItem>
+        <MenuItem value="es">
+          <StackLanguage label="ESPAÑOL" icon={es} />
+        </MenuItem>
       </Select>
     </FormControl>
+  );
+};
+
+const StackLanguage = (props: { icon: string; label: string }) => {
+  const { icon, label } = props;
+  return (
+    <Stack alignItems="center" direction="row" gap={1}>
+      <img src={icon} height="15dp" /> {label}
+    </Stack>
   );
 };
 
