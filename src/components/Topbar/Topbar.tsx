0b88c4f6aa46c748ce6252ab9e9d677775eafb47
Yoctoboy <alexis.reymann@hotmail.fr>
2025-07-14 22:56:51 +0200

    add responsive topbar for smaller screens

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -1,43 +1,11 @@
-import { AppBar, Box, Grid, Toolbar } from '@mui/material';
+import { useMediaQuery } from '@mui/material';
 
-import logo from '../../assets/logo.svg';
-import { useAuth } from '../../context/useAuth';
-import { useTranslationPrefix } from '../../utils/useTranslationPrefix';
-import { LogoutButton } from '../Login/LogoutButton';
-import LanguageSwitcher from './LanguageSwitcher';
-import { NavigationLink } from './NavigationLink';
+import { theme } from '../../theme';
+import { DesktopTopbar } from './DesktopTopbar';
+import { MobileTopbar } from './MobileTopbar';
 
 export const Topbar = () => {
-  const { token } = useAuth();
-  const { t } = useTranslationPrefix('pages');
-  const isLoggedIn = !!token;
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="sticky">
-        <Toolbar disableGutters>
-          <Grid container justifyContent="space-between" sx={{ flexGrow: 1 }}>
-            <Grid container spacing={5}>
-              <img height="30px" src={logo} alt="Logo" />
-              {isLoggedIn && (
-                <Grid container spacing={4} alignItems="center">
-                  <NavigationLink path="/dashboard" label={t('dashboard')} />
-                  <NavigationLink path="/alerts" label={t('alerts')} />
-                </Grid>
-              )}
-            </Grid>
-            <Grid
-              container
-              spacing={2}
-              alignItems="center"
-              justifyContent="space-around"
-              paddingRight="1rem"
-            >
-              <LanguageSwitcher />
-              {isLoggedIn && <LogoutButton />}
-            </Grid>
-          </Grid>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
+  const isMobile = useMediaQuery(theme.breakpoints.down('md'));
+
+  return isMobile ? <MobileTopbar /> : <DesktopTopbar />;
 };
