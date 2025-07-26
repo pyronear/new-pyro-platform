4b9ba28dfd5077ad93452660b823c11def3f3ab1
Yoctoboy <alexis.reymann@hotmail.fr>
2025-07-26 12:04:46 +0200

    fix useIsMobile to prevent collisions in topbar

--- src/utils/useIsMobile.ts
+++ src/utils/useIsMobile.ts
@@ -2,5 +2,5 @@ import { useMediaQuery, useTheme } from '@mui/material';
 
 export const useIsMobile = () => {
   const theme = useTheme();
-  return useMediaQuery(theme.breakpoints.down('sm'));
+  return useMediaQuery(theme.breakpoints.down('md'));
 };
