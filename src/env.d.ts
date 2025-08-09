f794a3375ea8c683f7728edc95b796a659a3a824
m-julio <55736935+fe51@users.noreply.github.com>
2025-08-09 18:40:49 +0200

    add cones and default value

--- src/env.d.ts
+++ src/env.d.ts
@@ -6,6 +6,9 @@ interface ImportMetaEnv {
   readonly VITE_DEFAULT_CAM_RANGE_KM: number;
   readonly VITE_ALERTS_LIST_REFRESH_INTERVAL_SECONDS: number;
   readonly VITE_HISTORY_NB_ALERTS_PER_PAGE: number;
+  readonly VITE_FILE_SITES_INFOS: string;
+  readonly VITE_FILE_SITES_LIVE_ACCESS: string;
+  readonly VITE_SITES_LIVE_PORT: number;
 }
 
 interface ImportMeta {
