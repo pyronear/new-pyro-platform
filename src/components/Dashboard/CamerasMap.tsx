4677d70bf5ac2db5b45be8f9780b70895b7988cc
Yoctoboy <alexis.reymann@hotmail.fr>
2025-08-24 15:39:42 +0200

    add map/cards interactions

--- src/components/Dashboard/CamerasMap.tsx
+++ src/components/Dashboard/CamerasMap.tsx
@@ -1,4 +1,5 @@
-import L from 'leaflet';
+import L, { Map as LeafletMap, Marker as LeafletMarker } from 'leaflet';
+import type { Dispatch, RefObject, SetStateAction } from 'react';
 import { MapContainer, TileLayer } from 'react-leaflet';
 
 import type { CameraType } from '../../services/camera';
@@ -7,9 +8,18 @@ import CameraMarkerMap from '../Common/Map/CameraMarkerMap';
 interface CameraMapProps {
   cameras: CameraType[];
   height?: string;
+  setMapRef: (map: LeafletMap) => void;
+  markerRefs: RefObject<Map<number, LeafletMarker>>;
+  setSelectedCameraId: Dispatch<SetStateAction<number | null>>;
 }
 
-export const CameraMap = ({ cameras, height = '100%' }: CameraMapProps) => {
+export const CameraMap = ({
+  cameras,
+  height = '100%',
+  setMapRef,
+  markerRefs,
+  setSelectedCameraId,
+}: CameraMapProps) => {
   const bounds = L.latLngBounds(cameras.map((c) => [c.lat, c.lon]));
   return (
     <MapContainer
@@ -17,13 +27,19 @@ export const CameraMap = ({ cameras, height = '100%' }: CameraMapProps) => {
       key={bounds.toBBoxString()}
       boundsOptions={{ padding: [20, 20] }}
       style={{ height, width: '100%', borderRadius: 4 }}
+      ref={(m: LeafletMap) => setMapRef(m)} // give map ref to parent component
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       {cameras.map((camera) => (
-        <CameraMarkerMap camera={camera} key={camera.id} />
+        <CameraMarkerMap
+          camera={camera}
+          key={camera.id}
+          markerRefs={markerRefs}
+          onClick={() => setSelectedCameraId(camera.id)}
+        />
       ))}
     </MapContainer>
   );
