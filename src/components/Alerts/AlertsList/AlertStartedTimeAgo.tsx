483b09213aeeda8701ecdda2d7e3b6e36bb8b3ef
Nicolas Descamps <nicolas.descamps@theodo.com>
2025-11-06 19:30:38 +0100

    feat: add hourglass icon next to time on alerts

--- src/components/Alerts/AlertsList/AlertStartedTimeAgo.tsx
+++ src/components/Alerts/AlertsList/AlertStartedTimeAgo.tsx
@@ -1,4 +1,5 @@
-import { Typography } from '@mui/material';
+import HourglassEmptyIcon from '@mui/icons-material/HourglassEmpty';
+import { Stack, Typography } from '@mui/material';
 
 import type { AlertType } from '../../../utils/alerts';
 import { formatTimeAgo } from '../../../utils/dates';
@@ -14,5 +15,10 @@ export const AlertStartedTimeAgo = ({ alert }: { alert: AlertType }) => {
 
   useRefreshEvery(60); // re-render every 60s
 
-  return <Typography variant="subtitle2">{formattedTimeAgo}</Typography>;
+  return (
+    <Stack direction="row" alignItems="center">
+      <HourglassEmptyIcon fontSize="inherit" color="disabled" />
+      <Typography variant="subtitle2">{formattedTimeAgo}</Typography>
+    </Stack>
+  );
 };
