98becaa1a5be6ac116a969109d4204f8d6f2a14d
Guillaume Sosson <guillaumes@hokla.com>
2025-11-06 21:22:26 +0100

    feat(useDetectAndPlaySoundForNewSequence): add hook to play sound when new alert

---
+++ src/components/Alerts/AlertsList/useDetectAndPlaySoundForNewSequence.ts
@@ -0,0 +1,44 @@
+import { useCallback, useEffect, useRef } from 'react';
+
+import type { SequenceType } from '../../../services/alerts';
+import { convertStrToEpoch } from '../../../utils/dates';
+import { useAlertSoundToggle } from './useAlertSoundToggle';
+
+export const useDetectAndPlaySoundForNewSequence = (
+  sequenceList: SequenceType[] | undefined,
+  dataUpdatedAt: number
+) => {
+  const { isAlertSoundOn, toggleSound, playSound } =
+    useAlertSoundToggle('/sounds/fire.mp3');
+
+  const previousDataUpdatedAtRef = useRef<number>(0);
+
+  const detectAndPlaySoundForNewSequences = useCallback(() => {
+    if (!sequenceList || sequenceList.length === 0) {
+      previousDataUpdatedAtRef.current = dataUpdatedAt;
+      return;
+    }
+
+    const previousDataUpdatedAt = previousDataUpdatedAtRef.current;
+
+    const hasNewSequence = sequenceList.some((sequence) => {
+      if (!sequence.started_at) {
+        return false;
+      }
+      const sequenceCreationTime = convertStrToEpoch(sequence.started_at);
+      return sequenceCreationTime >= previousDataUpdatedAt;
+    });
+
+    if (hasNewSequence) {
+      playSound();
+    }
+
+    previousDataUpdatedAtRef.current = dataUpdatedAt;
+  }, [sequenceList, dataUpdatedAt, playSound]);
+
+  useEffect(() => {
+    detectAndPlaySoundForNewSequences();
+  }, [detectAndPlaySoundForNewSequences]);
+
+  return { isAlertSoundOn, onSoundToggle: toggleSound };
+};
