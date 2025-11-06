15b4b47263fcf8d95f36b30dbc4e147217bba1a7
Nicolas Descamps <nicolas.descamps@theodo.com>
2025-11-06 20:07:58 +0100

    feat: change date format for different languages

--- src/components/History/HistoryList/HistoryFilters.tsx
+++ src/components/History/HistoryList/HistoryFilters.tsx
@@ -1,6 +1,7 @@
 import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { useTranslation } from 'react-i18next';
 
 import type { FiltersType } from '../../../utils/history';
 import { useTranslationPrefix } from '../../../utils/useTranslationPrefix';
@@ -10,8 +11,23 @@ interface HistoryFiltersType {
   setFilters: React.Dispatch<React.SetStateAction<FiltersType>>;
 }
 
+const getDateFormat = (locale: string) => {
+  switch (locale) {
+    case 'en':
+      return 'MM/DD/YYYY';
+    case 'fr':
+    case 'es':
+      return 'DD/MM/YYYY';
+    // TODO: traiter le cas général ? (en-US...)
+    default:
+      return 'DD/MM/YYYY';
+  }
+};
+
 export const HistoryFilters = ({ filters, setFilters }: HistoryFiltersType) => {
   const { t } = useTranslationPrefix('history');
+
+  const { i18n } = useTranslation();
 
   return (
     <LocalizationProvider dateAdapter={AdapterMoment}>
@@ -21,6 +37,7 @@ export const HistoryFilters = ({ filters, setFilters }: HistoryFiltersType) => {
         disableFuture
         value={filters.date}
         onChange={(newValue) => setFilters({ ...filters, date: newValue })}
+        format={getDateFormat(i18n.language)}
       />
     </LocalizationProvider>
   );
