78eda2242e28aa827d0758795c8117c393f01a48
Louis Groisne <162312590+louisGRS@users.noreply.github.com>
2025-11-22 10:37:34 +0100

    Feat/94/blinking mode (#140)
    * feat(blink): add blink button
    
    * feat(blink): add a blink overlay when blink mode enabled
    
    * feat(blink): close blink overview when pressing esc
    
    * feat(blink): add logo with proper colors
    
    * feat(blink): nicer closing cross
    
    * feat(blink): fix button style
    
    * feat(blink): add blinking effect on alert
    
    * feat(blink): only display the blink mode on desktop
    
    * refactor(blink): only render the blink overlay when in blink mode
    
    * refactor(close): use IconButton
    
    * refactor(colors): use theme colors
    
    * feat(localization): use i18n and add translations
    
    * refactor(return): use a single return

---
+++ src/components/Alerts/BlinkOverlay.tsx
@@ -0,0 +1,59 @@
+import HighlightOff from '@mui/icons-material/HighlightOff';
+import { Box, IconButton, useTheme } from '@mui/material';
+
+import logoLettersOrange from '@/assets/logo_letters_orange.png';
+
+interface BlinkOverlayProps {
+  closeOverlay: () => void;
+  hasAlert: boolean;
+}
+
+export const BlinkOverlay = ({ closeOverlay, hasAlert }: BlinkOverlayProps) => {
+  const theme = useTheme();
+
+  return (
+    <Box
+      sx={{
+        position: 'absolute',
+        left: 0,
+        top: 0,
+        width: '100vw',
+        height: '100vh',
+        backgroundColor: hasAlert
+          ? theme.palette.error.main
+          : theme.palette.primary.main,
+        zIndex: 100000,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        ...(hasAlert && {
+          animation: 'blinking 0.5s steps(1, end) infinite',
+          '@keyframes blinking': {
+            '0%, 100%': {
+              backgroundColor: theme.palette.error.main,
+            },
+            '50%': {
+              backgroundColor: theme.palette.common.black,
+            },
+          },
+        }),
+      }}
+    >
+      <Box
+        component="img"
+        src={logoLettersOrange}
+        alt="Pyro Logo"
+        sx={{
+          maxWidth: '80%',
+          maxHeight: '80%',
+          objectFit: 'contain',
+        }}
+      />
+      <Box position="absolute" top={32} right={32}>
+        <IconButton onClick={closeOverlay} aria-label="Exit blinking mode">
+          <HighlightOff sx={{ fontSize: '6rem', color: 'white' }} />
+        </IconButton>
+      </Box>
+    </Box>
+  );
+};
